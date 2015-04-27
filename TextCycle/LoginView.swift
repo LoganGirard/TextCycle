5ff266250afa5eecb074259fe403125d33af0ab4
Logan Girard <lgirard@Logans-MacBook-Pro.local>
2015-04-27 00:04:01 -0400

    login page skeleton, successful build

---
+++ TextCycle/LoginView.swift
@@ -0,0 +1,17 @@
+//
+//  LoginView.swift
+//  TextCycle
+//
+//  Created by Logan Girard on 4/26/15.
+//  Copyright (c) 2015 Logan Girard. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
