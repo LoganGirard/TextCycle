63f815f87b3842c4d9a14106253d4149a69358d6
Logan Girard <lgirard@Logans-MacBook-Pro.local>
2015-04-27 01:32:19 -0400

    working user login

--- TextCycle/LoginView.swift
+++ TextCycle/LoginView.swift
@@ -7,11 +7,62 @@
 //
 
 import UIKit
+import Parse
+import ParseUI
 
-class LoginView: UIViewController {
-   
+class LoginView: UIViewController, PFLogInViewControllerDelegate,
+    PFSignUpViewControllerDelegate {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        var user = PFUser.currentUser()
+        if user != nil {
+            println(user)
+           
+        } else {
+            println("No Logged in user")
+            var login: PFLogInViewController = PFLogInViewController()
+            login.fields = PFLogInFields.UsernameAndPassword | PFLogInFields.LogInButton |        PFLogInFields.SignUpButton
+            self.presentViewController(login, animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: Parse Login
+
+    func logInViewController(logInController: PFLogInViewController!, shouldBeginLogInWithUsername username: String!, password: String!) -> Bool {
+        return true
+    }
+    
+    func logInViewController(logInController: PFLogInViewController!, didLogInUser user: PFUser!) {
+        
+    }
+    
+    func logInViewController(logInController: PFLogInViewController!, didFailToLogInWithError error: NSError!) {
+    
+    }
+    
+    //Mark: Parse Signup
+    
+    func signUpViewController(signUpController: PFSignUpViewController!, shouldBeginSignUp info: [NSObject : AnyObject]!) -> Bool {
+        return true
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController!, didSignUpUser user: PFUser!) {
+        
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController!, didFailToSignUpWithError error: NSError!) {
+        
+    }
+    
+    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController!) {
+        
+    }
 }
